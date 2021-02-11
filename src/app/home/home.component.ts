90e0feacf5d554b1adb065918e9c45940650663b
Shubham Trivedi <shubham.trivedi@wikaad.com>
2021-02-11 16:31:38 +0530

    Home Page changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,16 @@ import { finalize, startWith, switchMap } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
   homeContent: any ;
+  serviceContent = [];
+  ghostSolutionList = [];
+  howItWorksContent = [];
+  faqContent = [];
   loading = false;
   constructor(private readonly contentsService: ContentService) { }
 
   ngOnInit(): void {
     this.contentsService.getPage('homepage').subscribe(res => {
+      this.loading = true;
       this.homeContent = res.data;
     });
   }
