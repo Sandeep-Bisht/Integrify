055964c74affd4e5939da2be5389ff7cf21d53f6
Shubham Trivedi <shubham.trivedi@wikaad.com>
2021-02-11 16:28:40 +0530

    Home Page Integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,23 +10,12 @@ import { finalize, startWith, switchMap } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
   homeContent: any ;
-  serviceContent = [];
-  ghostSolutionList = [];
-  howItWorksContent = [];
-  faqContent = [];
   loading = false;
   constructor(private readonly contentsService: ContentService) { }
 
   ngOnInit(): void {
     this.contentsService.getPage('homepage').subscribe(res => {
-      console.log(res);
-      this.loading = true;
-      this.serviceContent = res.data.ourServiceContent.iv;
-      this.ghostSolutionList = res.data.ghostSolutionContent.en;
-      this.howItWorksContent = res.data.howItWorksContent.en;
-      this.faqContent = res.data.faqContent.en;
       this.homeContent = res.data;
-      console.log(this.howItWorksContent);
     });
   }
 }
